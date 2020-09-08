e6ddcd7d408543fba57cda8e4b8e15864c039cb8
Daiane Souza <daianesou2704@gmail.com>
2020-09-07 23:35:29 -0400

    Updates

---
+++ precache-manifest.8fc8b91cea4be14e6a9e048998e83500.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51763478e5698f3d3bab3ff519c52c0",
+    "url": "/the-shoppies/index.html"
+  },
+  {
+    "revision": "c570cae077f25fe0fcf1",
+    "url": "/the-shoppies/static/css/main.1d3ad9d9.chunk.css"
+  },
+  {
+    "revision": "35061321fdc262a05534",
+    "url": "/the-shoppies/static/js/2.6df808c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-shoppies/static/js/2.6df808c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c570cae077f25fe0fcf1",
+    "url": "/the-shoppies/static/js/main.2c128d73.chunk.js"
+  },
+  {
+    "revision": "e9ec0706eaafa3de1aae",
+    "url": "/the-shoppies/static/js/runtime-main.b70cdf88.js"
+  }
+]);
